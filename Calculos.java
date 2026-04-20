8da61fa2caec2a87755426f6631519c8b675e000
Herick willian de lima Fragoso <herickwillianlima@gmail.com>
2026-04-20 15:44:19 -0300

    Funcao somar

---
+++ Calculos.java
@@ -0,0 +1,12 @@
+public class Calculos {
+    public int number;
+
+    public Calculos(int number){
+        this.number = number;
+    }
+
+    public int somar(int number,int number2){
+        int somar = number + number2;
+        return somar;
+    }
+}
