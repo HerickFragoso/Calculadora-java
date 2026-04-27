f526955344b05b18ef00f6bc95699eab7b94995c
Herick willian de lima Fragoso <herickwillianlima@gmail.com>
2026-04-27 12:06:44 -0300

    add testes ao github

--- main.java
+++ main.java
@@ -1,16 +1,22 @@
 import java.util.Scanner;
 
 public class main {
-    public static  void main(String []args){
+    public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("Digite o numero para descobrir a sua taboada");
-        int numero = scan.nextInt();
-        for (int i=0; i <= 10; i++){
-            int valoraexibir;
-            int numeroParaMultiplicar = i;
-            valoraexibir = numeroParaMultiplicar * numero;
-            System.out.println(numero + "X" + numeroParaMultiplicar + "=" + valoraexibir);
-        }
+
+        System.out.println("Digite o primeiro numero para somar:");
+        int numero1 = scan.nextInt();
+
+        System.out.println("Digite o segundo numero para somar:");
+        int numero2 = scan.nextInt();
+
+        Calculos calc = new Calculos(0);
+
+        int resposta = calc.somar(numero1, numero2);
+
+        System.out.println("Resultado da soma: " + resposta);
+
+        scan.close();
     }
 }
